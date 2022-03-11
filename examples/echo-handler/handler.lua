b0dea9fbb09afcd6d255fd971e020719720178d0
André <andrebq@gmail.com>
2022-03-11 19:56:13 +0100

    chore: add an example of how fake computation works

--- examples/echo-handler/handler.lua
+++ examples/echo-handler/handler.lua
@@ -1,3 +1,6 @@
 local handler = require("handler")
+local computations = require("computations")
+-- pretends that the system is doing an IO operation for 0.3 seconds
+computations.slow(0.3)
 handler.writeStatus(200)
 handler.writeBody("from LUA!")
